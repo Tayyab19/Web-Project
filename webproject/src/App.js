1e6fa23a48716b908f4303e8b51a01361f2ce956
anserbt <60539288+anserbt@users.noreply.github.com>
2022-04-20 22:22:55 +0500

    Added Users array in App.js

--- webproject/src/App.js
+++ webproject/src/App.js
@@ -7,7 +7,7 @@ import AllQuestionPage from "./questions_list_components/all_questions_page";
 import QuestionInfo from "./question_page_components/question_info";
 import AddQuestion from "./add_question_components/add_question";
 import Profile from "./profile_components/profile";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import Navbar from "./global_component/navbar";
 
 function App() {
@@ -278,19 +278,73 @@ function App() {
     },
   ]);
 
+  const [users, setUsers] = useState([
+    {
+      username: "BenHan1",
+      firstName: "Ben",
+      lastName: "Han",
+      reputation: 190,
+      badges: ["A", "B"],
+      email: "benhan@gmail.com",
+      addrress: "221-B Baker Street",
+      linkedIn: "benhan12",
+      gitHub: "benHan123",
+      company: "Softec",
+      preferredStack: "MERN",
+    },
+    {
+      username: "TenHang1",
+      firstName: "Ten",
+      lastName: "Hang",
+      reputation: 100,
+      badges: ["A", "B", "C"],
+      email: "tenhang@gmail.com",
+      addrress: "221-B Baker Street",
+      linkedIn: "tenHang12",
+      gitHub: "tenHang123",
+      company: "Softec",
+      preferredStack: "MERN",
+    },
+    {
+      username: "GenHang1",
+      firstName: "Gen",
+      lastName: "Hang",
+      reputation: 200,
+      badges: ["A", "B", "C", "D"],
+      email: "genhang@gmail.com",
+      addrress: "221-B Baker Street",
+      linkedIn: "genHang12",
+      gitHub: "genHang123",
+      company: "Softec",
+      preferredStack: "MERN",
+    },
+    {
+      username: "WenHang1",
+      firstName: "Wen",
+      lastName: "Hang",
+      reputation: 10,
+      badges: ["B", "C"],
+      email: "wenhang@gmail.com",
+      addrress: "221-B Baker Street",
+      linkedIn: "wenHang12",
+      gitHub: "wenHang123",
+      company: "Softec",
+      preferredStack: "MERN",
+    },
+  ]);
   const [filteredQuestions, setFilteredQuestions] = useState(questions);
 
   const [isFilter, setFilter] = useState(false);
 
   const getTags = () => {
-    let tags = []
-    questions.forEach(q => {
+    let tags = [];
+    questions.forEach((q) => {
       tags = tags.concat(q.tags);
     });
-  
+
     tags = [...new Set(tags)];
     return tags;
-  }
+  };
 
   const currUsername = {
     name: "Ten Hag",
@@ -301,15 +355,17 @@ function App() {
   };
 
   const updateQuestion = (q) => {
-    setQuestions(questions.map(question => {
-      question.question_id == q.question_id ? q : question;
-    }));
+    setQuestions(
+      questions.map((question) => {
+        question.question_id == q.question_id ? q : question;
+      })
+    );
     setFilteredQuestions(questions);
-  }
+  };
 
   const getQuestion = (id) => {
-    const q = filteredQuestions.filter(q => {
-      return (q.question_id == id)
+    const q = filteredQuestions.filter((q) => {
+      return q.question_id == id;
     });
     return q;
   };
@@ -320,32 +376,59 @@ function App() {
   };
 
   const applyFilter = (selectedTags) => {
-    selectedTags.length > 0 ?
-      setFilteredQuestions(questions.filter(q => {
-        return q.tags.some(tag => selectedTags.includes(tag))
-      }))
-    :
-    setFilteredQuestions(questions);
-  }
+    selectedTags.length > 0
+      ? setFilteredQuestions(
+          questions.filter((q) => {
+            return q.tags.some((tag) => selectedTags.includes(tag));
+          })
+        )
+      : setFilteredQuestions(questions);
+  };
 
-  useEffect(()=>{}, [filteredQuestions]);
+  useEffect(() => {}, [filteredQuestions]);
 
   useEffect(() => {
     isFilter ? null : setFilteredQuestions(questions);
-  }, [isFilter])
+  }, [isFilter]);
 
   return (
     <>
-    <Navbar />
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/questions/page/:pageNumber" element={<AllQuestionPage questions={filteredQuestions} username={currUsername} getTags={getTags} applyFilter={applyFilter} isFilter={isFilter} setFilter={setFilter}/>} />
-        <Route path="/questions/question/:qID" element={<QuestionInfo getQuestion={getQuestion} username={currUsername} />} />
-        <Route path="/questions/ask" element={<AddQuestion addQuestion={addQuestion} questionID={+questions.length+ +1} username={currUsername} />} />
-        <Route path="/profile/:username" element={<Profile />} />
-      </Routes>
-    </Router>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/questions/page/:pageNumber"
+            element={
+              <AllQuestionPage
+                questions={filteredQuestions}
+                username={currUsername}
+                getTags={getTags}
+                applyFilter={applyFilter}
+                isFilter={isFilter}
+                setFilter={setFilter}
+              />
+            }
+          />
+          <Route
+            path="/questions/question/:qID"
+            element={
+              <QuestionInfo getQuestion={getQuestion} username={currUsername} />
+            }
+          />
+          <Route
+            path="/questions/ask"
+            element={
+              <AddQuestion
+                addQuestion={addQuestion}
+                questionID={+questions.length + +1}
+                username={currUsername}
+              />
+            }
+          />
+          <Route path="/profile/:username" element={<Profile />} />
+        </Routes>
+      </Router>
     </>
   );
 }
