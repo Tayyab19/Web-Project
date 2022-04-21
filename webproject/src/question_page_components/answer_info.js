b48cb0a66fdade6f5a7da6189c8eaa469ee2c9cf
harisbst1 <mharis111@hotmail.com>
2022-04-22 02:37:28 +0500

    mods

--- webproject/src/question_page_components/answer_info.js
+++ webproject/src/question_page_components/answer_info.js
@@ -1,2 +1,20 @@
 import { useState } from "react"
 import { useParams } from "react-router-dom"
+
+const AnswerInfo = ({answer}) => {
+    return(
+        <div className="row">
+            <div className="answerButton col-sm-2" style={{textAlign:"center"}}>
+                <p>{answer.votes} likes</p>
+                <button className="btn btn-primary">upvote</button>
+            </div>
+            <div className="col-sm-10" style={{
+                display:"flex",
+                alignItems:"center"
+            }}>
+                {answer.string}
+            </div>
+        </div>
+    )
+}
+export default AnswerInfo;
